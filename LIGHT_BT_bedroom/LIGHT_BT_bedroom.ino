638b36cdf545e1dc929cb9a3d2dfd664835f519f
Karim Valeev <Adeptgt@yandex.ru>
2020-06-22 22:47:17 +0500

    В спальню добавлены 2 источника света и расширина группа основного света

--- LIGHT_BT_bedroom/LIGHT_BT_bedroom.ino
+++ LIGHT_BT_bedroom/LIGHT_BT_bedroom.ino
@@ -1,16 +1,27 @@
 #include "sav_button.h"
-SButton  lbut1(2,100,500,1000,500);
-int light1=4;
-int light2=5;
-//int light3=6;
+#include "Led.h"
+SButton  but_mainlight(2,100,500,1000,500);
+SButton  but_led(3,100,0,0,0);
+SButton  but_spot(4,100,0,0,0);
+Led ledlight(5,3);
+int spotlight=6;
+int mlight1=8;
+int mlight2=9;
+int mlight3=10;
+int mlight4=11;
 int lstatus=0;
 int ldirection=1; 
 void setup() {
   // put your setup code here, to run once:
-   lbut1.begin();
-   pinMode(light1, OUTPUT);
-   pinMode(light2, OUTPUT);
-   //pinMode(light3, OUTPUT);
+   but_mainlight.begin();
+   but_led.begin();
+   but_spot.begin();
+   ledlight.begin();
+   pinMode(spotlight, OUTPUT);
+   pinMode(mlight1, OUTPUT);
+   pinMode(mlight2, OUTPUT);
+   pinMode(mlight3, OUTPUT);
+   pinMode(mlight4, OUTPUT);
 
    
    Serial.begin(115200);
@@ -18,58 +29,83 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-switch( lbut1.Loop() ){
+ledlight.Loop();
+switch( but_led.Loop() ){
+      case SB_RELEASE: 
+         Serial.println("BUTTON led RELEASE");    
+         ledlight.OFF();
+         break;
+      case SB_CLICK: 
+         Serial.println("BUTTON led CLICK");
+         ledlight.ON();
+         break;
+   }
+
+switch( but_spot.Loop() ){  
+      case SB_RELEASE: 
+         Serial.println("BUTTON spotlight RELEASE");
+         digitalWrite(spotlight,LOW);
+         break;
+      case SB_CLICK: 
+         Serial.println("BUTTON spotlight CLICK");
+         digitalWrite(spotlight,HIGH);
+         break;
+   }
+
+
+
+
+switch( but_mainlight.Loop() ){
       case SB_AUTO_CLICK:
-      Serial.println("BUTTON 1 SB_AUTO_CLICK");
-      if(ldirection==0 && lstatus>0){lstatus--;}
-      else if(ldirection==1 && lstatus<2){lstatus++;}
-      break;
-      case SB_LONG_CLICK:
-         Serial.println("BUTTON 1 SB_LONG_CLICK");
-         if(ldirection==0 && lstatus>0){lstatus--;}
-         else if(ldirection==1 && lstatus<2){lstatus++;}
+         Serial.print("BUTTON 1 SB_AUTO_CLICK ");
+         if(ldirection==0 && lstatus>1){lstatus--;}
+         else if(ldirection==1 && lstatus<4){lstatus++;}
+         Serial.println(lstatus);
          break;
       case SB_RELEASE: 
          Serial.println("BUTTON 1 RELEASE");
-         //digitalWrite(light1,LOW);
-         // digitalWrite(light2,LOW);
-         // digitalWrite(light3,LOW);
          if(ldirection==0){ldirection=1;}
          else if(ldirection==1){ldirection=0;}
          break;
       case SB_CLICK: 
          Serial.println("BUTTON 1 CLICK");
-         if(lstatus==0){lstatus=2; ldirection=0;}
+         if(lstatus==0){lstatus=4; ldirection=0;}
          else {lstatus=0; ldirection=1;}
-         //digitalWrite(light1,HIGH);
-         // digitalWrite(light2,HIGH);
-         //digitalWrite(light3,HIGH);
          break;  
    }
 
 switch (lstatus)
 {
  case 0:
-    digitalWrite(light1,LOW);
-    digitalWrite(light2,LOW);
-   // digitalWrite(light3,LOW);
+    digitalWrite(mlight1,LOW);
+    digitalWrite(mlight2,LOW);
+    digitalWrite(mlight3,LOW);
+    digitalWrite(mlight4,LOW);
     break;
  case 1:
-    digitalWrite(light1,HIGH);
-    digitalWrite(light2,LOW);
-   // digitalWrite(light3,LOW);
+    digitalWrite(mlight1,HIGH);
+    digitalWrite(mlight2,LOW);
+    digitalWrite(mlight3,LOW);
+    digitalWrite(mlight4,LOW);
     break;
  case 2:
-    digitalWrite(light1,HIGH);
-    digitalWrite(light2,HIGH);
-   // digitalWrite(light3,LOW);
+    digitalWrite(mlight1,HIGH);
+    digitalWrite(mlight2,LOW);
+    digitalWrite(mlight3,LOW);
+    digitalWrite(mlight4,HIGH);
     break;
-// case 3:
-//    digitalWrite(light1,HIGH);
-//    digitalWrite(light2,HIGH);
-//    digitalWrite(light3,HIGH);
-//    break;                   
+ case 3:
+    digitalWrite(mlight1,HIGH);
+    digitalWrite(mlight2,HIGH);
+    digitalWrite(mlight3,LOW);
+    digitalWrite(mlight4,HIGH);
+    break;
+ case 4:
+    digitalWrite(mlight1,HIGH);
+    digitalWrite(mlight2,HIGH);
+    digitalWrite(mlight3,HIGH);
+    digitalWrite(mlight4,HIGH);
+    break;                    
 }
 
 
